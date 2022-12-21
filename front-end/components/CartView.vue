2434897f11ff3d23b5f9f3ee5186ac1dfaadc33f
Lorenzo <burgos.lorenzo15@gmail.com>
2022-12-21 14:19:17 -0300

    feat: cart not working as expected

--- front-end/components/CartView.vue
+++ front-end/components/CartView.vue
@@ -4,13 +4,18 @@
     <br />
     <h2 class="pl-4">produtos:</h2>
     <div class="col grid col-span-1">
-      <div v-for="product in products" :key="product.id">
+      <div v-for="product in products" :key="product.id"
+      class="ml-4 mb-4 w-60 pt-3 bg-slate-50 hover:bg-slate-300 hover:drop-shadow-2xl">
         <NuxtLink to="/product"
           ><img src="http://placekitten.com/200/300" class="pl-4"
         />
         <p class="pl-4">
           {{ product.name }}
         </p>
+          <button
+          class="flex flex-col text-left items-center justify-center mt-5 p-4 rounded-full bg-lime-400">
+            remover do carrinho
+          </button>
         </NuxtLink>
       </div>
     </div>
