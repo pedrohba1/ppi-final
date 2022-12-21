762e72efc38034142492221a684f7512c4560940
Lorenzo <burgos.lorenzo15@gmail.com>
2022-12-21 10:56:20 -0300

    feat: cart not working 3

--- front-end/mock/handlers/getCart.ts
+++ front-end/mock/handlers/getCart.ts
@@ -1,16 +1,18 @@
 import { rest } from 'msw'
 
-export const getProduct = rest.get('/cart', (req, res, ctx) => {
-  const productId = req.url.searchParams.get('id')
+export const getProducts = rest.get('/cart', (_, res, ctx) => {
+  const products = [1, 2, 3].map((item) => ({
+    id: item,
+    name: 'carro de rolimã',
+    description: 'descrição',
+    price: '100.00',
+    vendor: 'Bufulin',
+    amount: 5,
+  }))
 
   return res(
     ctx.json({
-      id: productId,
-      name: 'carro de rolimã',
-      description: 'descrição',
-      price: '100.00',
-      vendor: 'Bufulin',
-      amount: 1,
+      products,
     })
   )
 })
