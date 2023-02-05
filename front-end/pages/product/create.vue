4cc285fa04cbd038bb826bb2a591abba06d7fc2e
pedro bufulin <pedro.almeida@ufu.br>
2023-02-05 19:17:43 -0300

    create product page

---
+++ front-end/pages/product/create.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1> test</h1>
+  
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'ProductCreatePage',
+})
+</script>
